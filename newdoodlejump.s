83a19aeb0f1dc46b54b436e8a9bc6554817b3bf7
Jefferson(Zhe Fan) Li <jeffersonli.li@mail.utoronto.ca>
2020-11-22 21:01:32 -0500

    stable scrolling

--- newdoodlejump.s
+++ newdoodlejump.s
@@ -46,7 +46,8 @@
 	positionStruct: .word 56, 20, -16, displayAddress,  # current x,y | acceleration | previous pixel position to repaint
 	                     # x must manuely be word aligned (inc by 4), y is automatically word aligned (inc by 1)
 	                     
-	platforms: .space 10 # max of 10 platforms can be existing at 1 frame
+	platforms: .space 20 # max of 10 platforms can be existing at 1 frame, + 10 platforms last position
+
 	difficulty: .word 0 # increments at a set pace, max is level 8
 .text
 	# global registers
@@ -54,6 +55,7 @@
          # game states
       	  la $s1, positionStruct #spaced by 4
 	  la $s2, platforms #spaced by 4
+	  
 	  lw $s3, difficulty  
 	 # dims (all already spaced by 4) 
 	  lw $t4, screenUnit
@@ -72,8 +74,14 @@
 	 # DO NOT USE S6, 7
 	# END global registers
 Main:
-	jal FillBackground # fill backdrop first
+	jal WipeBoard # fill backdrop first
 	jal initPads # init platform
+	InitOnPress: # waits for user imput before startingthe game 
+		li $v0, 32
+		li $a0, 50
+		syscall
+		lw $t0, 0xffff0000 
+		bne $t0, 1, InitOnPress
 GameLoop:
 	jal Catch # catch gameover 
 	jal UpdateDoodleVertical # update verticality of doodle
@@ -82,7 +90,7 @@ GameLoop:
 	jal DrawPadAndHitTest # draw all pads
 
 	li $v0, 32
-	li $a0, 15 # speed todo increment as game goes on 
+	li $a0, 50 # speed todo increment as game goes on 
 	syscall
 	j GameLoop
 	
@@ -126,38 +134,38 @@ OnMove: # only handle horizontal user movement and redraw, do not redraw if no m
 DrawDoodle: 
 # draws sprite based on position struct
 # todo support turning around
-
-	#replace old paint
-	lw $t5, background
-	
-	lw $t0, 12($s1)
-	sw $t5, 0($t0)
-	sw $t5, 4($t0)
-	
-	# paint new position
-	lw $t0, 0($s1) # x
+	# calc new pos
+	lw $t2, 0($s1) # x
     	lw $t1, 4($s1) # y
 	
 	mult $t1, $s4 
 	mflo $t3 # y * width
-	add $t4, $t3, $t0 # y + x
+	add $t4, $t3, $t2 # y + x
 	add $t4, $t4, $s0, # coord + address base
-
+	
+	
+	#replace old paint
+	lw $t0, 12($s1)
+	lw $t5, background
+	sw $t5, 0($t0)
+	sw $t5, 4($t0)
+	
+	# new paint
 	lw $t5, baigeGreen
-	# fill colours 
 	sw $t5, ($t4)
 	sw $t5, 4($t4)
 
 	#save last location to paint over
         sw $t4, 12($s1)
-        	
+        
+        ddret:
 	jr $ra # return
 
 initPads:# called once on init, randomly fills the platforms array
 	li $t0, 9 #counter
 	ipWhile:
 		li $v0, 42  #generates the random number.
-		li $a1, 500  #random num between 0 and 1000
+		li $a1, 800  #random num between 0 and 1000
     		syscall
     		li $t1, 4 
     		mult $a0, $t1 # a0 is out actual rng number
@@ -167,8 +175,14 @@ initPads:# called once on init, randomly fills the platforms array
     		mult $t0, $t1 
     		mflo $t4 # array index 
     		add $t4, $t4, $s2 
-    		
     		sw $t3, 0($t4)
+    		
+    		addi $t2, $t0, 10
+    		mult $t2, $t1 
+    		mflo $t4 # array index 
+    		add $t4, $t4, $s2 
+    		sw $s0, 0($t4)
+    		
     		beqz $t0, ipEnd
     		addi $t0, $t0,  -1
     		j ipWhile
@@ -181,16 +195,16 @@ DrawPadAndHitTest: # draws pads from platforms
 	sub $t0, $t0, $s3 # subtract number of pads from difficulty (more difficult = less pads)  
 	
 	li $t1, 4 
-	lw $t5, padGreen # color to fill
 	lw $t7, 12($s1) # last location of doodle
 	addi $t7, $t7, -6 # centering factor
+	add $t7, $t7, $s4 # minus by 1 row
 	lw $s6, 8($s1) # current accel of doodle
 	dpfWhile:
     		mult $t0, $t1 
     		mflo $t4 # array index 
     		add $t4, $t4, $s2
     		
-    		lw $t4, 0($t4) # t3 has pad coords
+    		lw $t4, 0($t4) # t4 has pad coords
     		
     		sub $t6, $t4, $t7
     		abs $t6, $t6 # absolute difference of pad and doodle
@@ -210,31 +224,60 @@ DrawPadAndHitTest: # draws pads from platforms
 			sw $s7, 8($s1) # accel
 		SkipAcc:
 		
-		# fill colours 
+		addi $t2, $t0, 10 #find our past position
+    		mult $t2, $t1 
+    		mflo $t2 # array index 
+    		add $t3, $t2, $s2 
+    		lw $t2, 0($t3) 
+    		
+    		# replace old colours
+    		lw $t5, background # color to fill
+    		sw $t5, ($t2)
+		sw $t5, 4($t2)
+		sw $t5, 8($t2)
+		sw $t5, 12($t2)
+		
+		# fill new colours 
+    		lw $t5, padGreen # color to fill
 		sw $t5, ($t4)
 		sw $t5, 4($t4)
 		sw $t5, 8($t4)
 		sw $t5, 12($t4)
+		
+		sw $t4, 0($t3) # save prev position
+    		
+    		dpfWhileEnd:
     		beqz $t0, dpfEnd
-    		addi $t0, $t0,  -1
+    		addi $t0, $t0,  -1    		
     		j dpfWhile
     	dpfEnd:
     	lw $t0, 0($sp) #get ra
-	addi $sp, $sp, 4 #pop y and x off
+	addi $sp, $sp, 4 
 	
     	jr $t0
 
 UpdateDoodleVertical: # called to update the doodle position based on velocity
+	addi $sp, $sp, -4
+	sw $ra, 0($sp) # push ra on stack
+
 	lw $t0, 8($s1) # accell
 	lw $t1, 4($s1) # y coord
 	
-	bltz $t0 HeadDown
-	j HeadUp
-	HeadDown:
+	bltz $t0 HeadUp
+	j HeadDown
+	HeadUp:
+		ble $t1, 10, scroll
+		j goup
+		scroll:
+			jal ScrollBoard
+			lw $t0, 8($s1) # accell
+			lw $t1, 4($s1) # y coord
+			j udpdone
+		goup:
 		addi $t1, $t1, -1
         	sw $t1, 4($s1) # go up 
         	j udpdone
-	HeadUp:
+	HeadDown:
 		addi $t1, $t1, 1
         	sw $t1, 4($s1) # go down
         udpdone:
@@ -242,9 +285,12 @@ UpdateDoodleVertical: # called to update the doodle position based on velocity
         addi $t0 , $t0 , 1
 	sw $t0 , 8($s1) # decrement accell 
         
-	jr $ra
+	lw $ra, 0($sp) #get ra
+	addi $sp, $sp, 4 
 	
-FillBackground: # this function fills entire play area with background colour
+    	jr $ra
+	
+WipeBoard: # this function fills entire play area with background colour
 
 	lw $t5, background
 	
@@ -262,6 +308,32 @@ FillBackground: # this function fills entire play area with background colour
 		j FBwhile
 	FBend:
 	jr $ra
+
+ScrollBoard:
+	
+	addi $sp, $sp, -4
+	sw $ra, 0($sp) # push ra on stack
+	li $t7, 9 # counter
+	sub $t7, $t7, $s3 # subtract number of pads from difficulty (more difficult = less pads)  
+	li $t1, 4 
+	sbWhile:
+    		mult $t7, $t1 
+    		mflo $t4 # array index 
+    		add $t4, $t4, $s2
+    		
+    		lw $t3, 0($t4) # t3 has pad coords
+    		
+    		add $t3, $t3, $s4 # add width to pad position to move down by 1
+		sw $t3, 0($t4) # save the coord 
+		
+    		beqz $t7, dpfEnd
+    		addi $t7, $t7,  -1
+    		j sbWhile
+    	sbEnd:
+
+    	lw $ra, 0($sp) #get ra
+	addi $sp, $sp, 4 
+    	jr $ra
 
 Catch:
 
